09b5185c49ee4cf276ec27a471c80aadfe390fe2
Rutvik Raut <rutvikr@cdac.in>
2026-02-25 17:55:55 +0530

    added footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/common/header/header/header";
+import { Footer } from "./components/common/footer/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
